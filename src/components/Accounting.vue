7e88f1839246460befd481054af9031016ab04d6
fumiya <b5345@oic.jp>
2020-09-21 21:48:41 +0900

    firestoreにaccountデータを登録処理の追加

--- src/components/Accounting.vue
+++ src/components/Accounting.vue
@@ -3,23 +3,36 @@
     <h2>確認</h2>
     <table>
       <tr>
-        <th>メニュー</th>
-        <th>注文数</th>
-        <th>金額</th>
+        <th class="cuisine_name">メニュー</th>
+        <th class="cuisine_name">注文数</th>
+        <th class="cuisine_price">金額</th>
       </tr>
       <tr class="items" v-for="item in items" :key="item.name">
         <td class="cuisine_name">{{ item.name }}</td>
         <td class="cuisine_num">{{ item.num }}</td>
         <td class="cuisine_price">¥{{ item.price }}</td>
       </tr>
+      <tr class="sum_price">
+        <td></td>
+        <td>
+          <b>合計</b>
+        </td>
+        <td>
+          <b>¥{{ sumPrice() }}</b>
+        </td>
+      </tr>
     </table>
 
-    <button @click="d">いいえ</button>
-    <button>はい</button>
+    <div class="account_proceed">
+      <router-link class="to_menu" to="/menu" tag="button">追加注文</router-link>
+      <button class="call_staff" @click="runAccount">お会計する</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { db } from "@/plugins/firebase";
+
 export default {
   name: "account",
   data: function () {
@@ -43,16 +56,64 @@ export default {
       ],
     };
   },
+  methods: {
+    sumPrice() {
+      let sum = 0;
+      this.items.forEach((item) => {
+        sum += item.price;
+      });
+      return sum;
+    },
+    runAccount() {
+      console.log("runAccount");
+      db.collection("account/").add({
+        items: [
+          {
+            name: "麻婆豆腐",
+            num: 1,
+            price: 800,
+          },
+          {
+            name: "油淋鶏",
+            num: 1,
+            price: 700,
+          },
+          {
+            name: "炒飯",
+            num: 1,
+            price: 750,
+          },
+        ],
+        sum_price: this.sumPrice(),
+      });
+    },
+  },
 };
 </script>
 
 <style>
+table {
+  width: calc(100% - 60px);
+  text-align: left;
+}
 .accounting {
   margin-left: 30px;
   margin-right: 30px;
 }
 .cuisine_num,
 .cuisine_price {
+  margin-left: 30px;
+}
+.items {
+  margin-top: 30px;
+}
+.sum_price {
+  font-size: 18px;
+}
+.account_proceed {
+  margin-top: 20px;
+}
+.call_staff {
   margin-left: 20px;
 }
 </style>
