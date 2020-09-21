3ed12fedc17d0a49c1c474b4da603905c0eaeac3
fumiya <b5345@oic.jp>
2020-09-22 00:38:36 +0900

    レストラン名とシートを表示するように変更

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="home">
-    <h2>お店の名前OO席</h2>
-
     <h1>いらっしゃいませ</h1>
-
+    <h2 v-if="restaurant !== null">{{ restaurant.info.name }}{{ seatId }}席</h2>
     <p>何名様ですか？</p>
-
     <div class="customer_num_container">
-      <button class="customer_minus-button" @click="minus">−</button>
+      <v-btn color="success" text class="customer_minus-button" @click="minus">−</v-btn>
       <div class="customer_num">{{ customer_num }}</div>
-      <button class="customer_plus-button" @click="plus">＋</button>
+      <v-btn color="error" text class="customer_plus-button" @click="plus">＋</v-btn>
     </div>
-
     <p>お車でお越しですか？</p>
-    <input type="radio" value="car" id="car" class="car" v-model="transportation">
+    <input type="radio" value="car" id="car" class="car" v-model="transportation" />
     <label for="car">はい</label>
-    <input type="radio" value="etc" id="etc" class="etc" v-model="transportation">
+    <input type="radio" value="etc" id="etc" class="etc" v-model="transportation" />
     <label for="etc">いいえ</label>
-
-    <button class="to_menu">メニューに進む</button>
-
+    <v-btn to="menu" class="to_menu">メニューに進む</v-btn>
   </div>
 </template>
 
@@ -29,8 +23,21 @@ export default {
   data: function() {
    return{
      customer_num: 0,
-     transportation: ""
+     transportation: "",
+     restaurants: null,
+     restaurant: null,
+     seatId: ""
    }
+  },
+  created: async function(){
+    const restaurantId = this.$route.query.restaurantId;
+    this.seatId = this.$route.query.seatid
+    const url = `https://omochikaeri.com/api/1.0/restaurants/`;
+    const response = await fetch(url, {mode: 'cors'})
+    const responseJson = await response.json()
+    this.restaurants = responseJson.payload.restaurants
+    this.restaurant = this.restaurants.find(restaurant => restaurant.id === restaurantId)
+
   },
   methods: {
     minus() {
@@ -47,6 +54,15 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  margin-top: 30px;
+  font-weight: normal;
+  font-size: 35px;
+}
+h2 {
+  font-weight: normal;
+  font-size: 30px;
+}
 .home {
   margin-left: 30px;
   margin-right: 30px;
@@ -57,6 +73,7 @@ export default {
 }
 .customer_minus-button {
   width: 40px;
+  font-size: 30px !important;
 }
 .customer_num {
   margin-left: 30px;
@@ -64,11 +81,14 @@ export default {
 .customer_plus-button {
   margin-left: 30px;
   width: 40px;
+  font-size: 30px !important;
 }
 .etc {
   margin-left: 30px;
 }
 .to_menu {
+  width: 150px;
+  padding-top: 8px !important;
   font-size: 25px;
   margin-top: 30px;
   display: block;
