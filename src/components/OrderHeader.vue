0aca9a2976bfe4a9288a9ffdb66a8a92fbeb26bb
TakaWakiyama <taka.wakiyama@automationlab.co>
2020-09-22 04:09:16 +0900

    componentify header

---
+++ src/components/OrderHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      <h1>メニュー</h1>
+    </v-col>
+    <v-spacer></v-spacer>
+    <v-col>
+      <v-btn rounded large color="error" @click="$emit('click')" class="btn">{{ btnString }}</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    btnString: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
