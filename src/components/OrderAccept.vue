7341c1319c707042530ff5ce36d5b14190c9d864
TakaWakiyama <taka.wakiyama@automationlab.co>
2020-09-22 00:34:59 +0900

    add default color

--- src/components/OrderAccept.vue
+++ src/components/OrderAccept.vue
@@ -1,51 +1,50 @@
 <template>
   <div id="app" class="container">
+    <v-dialog
+      v-model="showDialog"
+      dark
+    >
+      <v-card>
+        <v-toolbar dark>
+        </v-toolbar>
+      </v-card>
+    </v-dialog>
     <h1>Menu List</h1>
       <v-row v-for="(menu, key) in menus" :key="key">
         <v-col cols="6">
            <v-img
               :src="menu.itemInfo.image"
-              width="20%"
+              width="60%"
               contain
               class="grey darken-4"
             />
         </v-col>
         <v-col cols="6">
-          <span class="name">{{menu.itemInfo.name}}</span>
-          <span class="price">{{menu.price.price}}円</span>
-          <p>{{menu.itemInfo.description}}</p>
+          <span class="name">{{ menu.itemInfo.name }}</span>
+          <span class="price">{{ menu.price.price }}円</span>
+          <span>{{ menu.itemInfo.description }}</span>
+          <v-btn color="primary" click="onOpenDialog(key)">追加</v-btn>
         </v-col>
       </v-row>
   </div>
 </template>
-<style scoped>
-li{
-  padding: 30px auto;
-  display: flex;
-}
-li.img{
-  width: 360px;
-}
-li.name{
-  font-size: 24px;
-}
-li.price{
-  font-size: 24px;
-}
-</style>
 <script>
 // https://omochikaeri.com/api/1.0/restaurants/5WmWRzIbF7ilJcigW7Vx/menus
 // https://omochikaeri.com/api/1.0/restaurants/{restaurant_id}/menus
 
 export default {
-  components: {
-  },
   data() {
     return {
-      menus: []
+      menus: [],
+      showDialog: false,
+      numOrder: 0,
     }
   },
   methods: {
+    onOpenDialog(index) {
+      console.log({index})
+      this.showDialog=true
+    }
   },
   mounted: function(){
     const restaurant_id = this.$route.query.restaurantId;
