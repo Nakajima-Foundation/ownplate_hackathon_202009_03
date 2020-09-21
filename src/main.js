b9e3843f611f15d9b13056c11e1f64861ebacdcb
fumiya <b5345@oic.jp>
2020-09-21 21:47:44 +0900

    firebaseの追加

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
-// import "@/plugins/axios";
+import "@/plugins/axios";
+import "@/plugins/firebase"
 
 Vue.config.productionTip = false
 
