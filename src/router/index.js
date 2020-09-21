72021a52fb21721758537fd4a0f57fcd7466eaf0
fumiya <b5345@oic.jp>
2020-09-21 16:04:34 +0900

    修正版

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import routes from "./router";
-import "@/plugins/axios";
 
 const originalPush = Router.prototype.push;
 Router.prototype.push = function push(location) {
